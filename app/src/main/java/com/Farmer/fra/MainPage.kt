468769129b9a192433f91864cb88d0a519f90c9e
Mihir Jain <jainsuresh713@gmail.com>
2023-05-28 09:41:18 +0530

    Erp Commit

--- app/src/main/java/com/Farmer/fra/MainPage.kt
+++ app/src/main/java/com/Farmer/fra/MainPage.kt
@@ -18,35 +18,10 @@ import com.google.firebase.auth.FirebaseAuth
 class MainPage : AppCompatActivity() {
     private lateinit var auth : FirebaseAuth
     private lateinit var signOut : Button
-    lateinit var drawerLayout: DrawerLayout
-    lateinit var coordinatorLayout: CoordinatorLayout
-    lateinit var toolbar: androidx.appcompat.widget.Toolbar
-    lateinit var frameLayout: FrameLayout
-    lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
-        drawerLayout = findViewById(R.id.drawer_layout)
-        coordinatorLayout = findViewById(R.id.coordinator)
-        toolbar = findViewById(R.id.toolbar)
-        frameLayout = findViewById(R.id.frame)
-        navigationView = findViewById(R.id.navigation_bar)
-
-        val DrawerToggle = ActionBarDrawerToggle(this@MainPage,
-            drawerLayout , R.string.open_drawer, R.string.close_drawer)
-        drawerLayout.addDrawerListener(DrawerToggle)
-        DrawerToggle.syncState()
-
-        navigationView.setNavigationItemSelectedListener {
-            when(it.itemId)
-            {
-                R.id.dash_profile->{
-                    Toast.makeText(this@MainPage,"clicked to profile",Toast.LENGTH_SHORT).show()
-                }
-            }
-            return@setNavigationItemSelectedListener true
-        }
 
         auth = FirebaseAuth.getInstance()
         signOut = findViewById(R.id.signout)
@@ -55,24 +30,10 @@ class MainPage : AppCompatActivity() {
             startActivity(Intent(this, Login::class.java))
 
         }
-        setupToolbar()
+
 
     }
-    fun setupToolbar(){
-        setSupportActionBar(toolbar)
-        supportActionBar?.title="Farmer"
-        supportActionBar?.setHomeButtonEnabled(true)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if(id == android.R.id.home)
-        {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
-        return super.onOptionsItemSelected(item)
-    }
 
 
 }
